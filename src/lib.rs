0c1fdbda0c4f0df48bb753a6f99ae96a36ef29bf
Paulo <jimgr@riseup.net>
2024-04-11 18:53:29 +0200

    Added  json generation. Removed unwrap

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,93 @@
-use std::string::FromUtf8Error;
+use urlencoding::decode;
 
-pub fn decode_url(url_string: String) -> Result<String, FromUtf8Error>{
-    use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
-    let bytes_url = engine::GeneralPurpose::new(
-                 &alphabet::URL_SAFE,
-                 general_purpose::NO_PAD)
-        .decode(url_string).unwrap();
-    String::from_utf8(bytes_url)
+use std::fmt;
+
+pub struct ParsingError;
+
+impl fmt::Display for ParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error with file") // user-facing output
+    }
 }
 
-pub fn clear_ss(url_string: String) -> String{
-    let url_split = if url_string.contains("ss:"){
+impl fmt::Debug for ParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{ file: {}, line: {} }}", file!(), line!()) // programmer-facing output
+    }
+}
+
+pub struct ShadowSocksRaw {
+    pub key: String,
+    pub name: String
+}
+#[macro_use] extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+pub struct ShadowSocksJSON {
+    server: String,
+    server_port: u32,
+    local_port: u32,
+    password: String,
+    method : String,
+    remarks: String,
+}
+
+
+
+impl ShadowSocksJSON {
+    pub fn from_decode_string(decode_string: String, name: String) -> Result<ShadowSocksJSON, ParsingError> {
+        let text: Vec<&str> = decode_string.split("@").collect();
+        let server_address_port: Vec<&str> = text.last().ok_or(ParsingError)?.split(":").collect();
+        let server_port = match server_address_port.last().ok_or(ParsingError)?.parse::<u32>(){
+            Ok(data) => Ok(data),
+            Err(_) => Err(ParsingError)
+        }?;
+        let server_method_password: Vec<&str> = text.first().ok_or(ParsingError)?.split(":").collect();
+
+        Ok(ShadowSocksJSON {
+            server: server_address_port.first().ok_or(ParsingError)?.to_string(),
+            server_port: server_port,
+            local_port: 1000_u32,
+            password: server_method_password.last().ok_or(ParsingError)?.to_string(),
+            method: server_method_password.first().ok_or(ParsingError)?.to_string(),
+            remarks: name
+        })
+    }
+}
+pub fn decode_url(url_string: String) -> Result<String, ParsingError>{
+    use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
+    let bytes_url = match  engine::GeneralPurpose::new(
+                 &alphabet::URL_SAFE,
+                 general_purpose::NO_PAD)
+        .decode(url_string){
+            Ok(data) => Ok(data),
+            Err(_) => Err(ParsingError)
+        }?;
+    match String::from_utf8(bytes_url) {
+        Ok(data) => Ok(data),
+        Err(_) => Err(ParsingError)
+    }
+}
+
+pub fn clear_ss(url_string: String) -> ShadowSocksRaw{
+    let url_split = if url_string.contains("ss:"){ //Delete ss:// if ss address contains it
         url_string.split("ss://").collect::<Vec<&str>>()[1]
         
     }
     else{
         &url_string
     };
-    if url_split.contains("#"){                       
-        url_split.split("#").collect::<Vec<&str>>()[0]     
+
+    if url_split.contains("#"){       
+        ShadowSocksRaw {
+            key: url_split.split("#").collect::<Vec<&str>>()[0].to_string(),
+            name: decode(url_split.split("#").collect::<Vec<&str>>()[1]).expect("UTF-8").replace("+", " ").to_string()
+        }               
     }       
     else{
-        &url_split
-    }.to_string()      
-
-
+        ShadowSocksRaw {
+            key: url_split.split("#").collect::<Vec<&str>>()[0].to_string(),
+            name: "No_Name".to_string()
+        }
+    }    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use ss_to_json_outline;
+use serde_json;
  
 fn main() {
     use std::env;
     let args: Vec<String> = env::args().collect();
     if args.len() > 1{
-        let text = ss_to_json_outline::decode_url(ss_to_json_outline::clear_ss(args[1].clone())).unwrap();
-        println!("{}", text);
+
+        let shadow_socks_raw = ss_to_json_outline::clear_ss(args[1].clone());
+
+        let text: String = ss_to_json_outline::decode_url(shadow_socks_raw.key).unwrap();
+
+        let json = ss_to_json_outline::ShadowSocksJSON::from_decode_string(text, shadow_socks_raw.name);
+        print!("{}", serde_json::to_string(&json.unwrap()).unwrap());
     }
     else{
         println!("usage: ss-to_json *your_key*");
